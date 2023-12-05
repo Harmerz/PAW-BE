2dbc4d94341c0018db533814ef47ce7215bddda5
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-12-05 10:58:52 +0700

    feat : delete routes

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,6 @@ const controller = require('../controllers/user')
 const express = require('express')
 const router = express.Router()
 
-router.get('/all', controller.allAccess)
 router.get('/user', [authJwt.verifyToken], controller.userData)
 router.post('/user', [authJwt.verifyToken], controller.userInput)
 module.exports = router
